9f74ab533ac420fd8253d01a63aa6730fc8b7ddd
Maharshi Pandya <maharshi.pandya2002@gmail.com>
2022-01-29 20:18:46 +0530

    :hammer: Excersice for dplyr::filter function

--- explore_5_data_transformation/data_transform.R
+++ explore_5_data_transformation/data_transform.R
@@ -3,18 +3,101 @@ library(tidyverse)
 
 ?flights
 
+summary(flights)
+head(flights)
+
 flights
 View(flights)
 
-# 5 Verbs for dplyr
+# 5 Verbs for data manipulation (dplyr)
 # filter()    : pick observations based on values
 # arrange()   : reorder the rows
 # select()    : pick variables by their names
 # mutate()    : create new variables as function of existing 
 # summarise() : collapse many values down to a single summary
 
-# These verbs can be used 
+# These verbs can be used with group_by() which changes the
+# scope of each function to operate on group-by-group basis instead
+# of the entire dataframe at once
 
+# Workings of Verbs:
+# 1) The first argument is the data frame
+# 2) The subsequent arguments tells what to do with the dataframe
+# 3) The result is a new data frame
 
-summary(flights)
-head(flights)
+# ---------- Filter ----------------
+?dplyr::filter
+
+# arguments are joined with & by default
+(jan1 <- dplyr::filter(flights, month == 1, day == 1))
+(dec25 <- dplyr::filter(flights, month == 12, day == 25))
+
+# gives FALSE
+sqrt(2) ^ 2 == 2
+
+# using near gives TRUE
+?dplyr::near
+near(sqrt(2) ^ 2, 2)
+
+# filter flights in november or december
+dplyr::filter(flights, month == 11 | month == 12)
+
+# shorthand is %in%
+dplyr::filter(flights, month %in% c(11, 12))
+
+# demorgan's law
+# !(x & y) is equal to !x | !y
+# !(x | y) is equal to !x & !y
+
+# Filter flights such that their arrival or departure delay
+# isn't more than two hours
+# We have to find:
+# !(arr_delay > 120 | dep_delay > 120)
+
+# There are two ways:
+dplyr::filter(flights, !(arr_delay > 120 | dep_delay > 120))
+dplyr::filter(flights, arr_delay <= 120, dep_delay <= 120)
+
+# determine if value is NA
+# gives true
+z <- NA
+is.na(z)
+
+# If we want to include missing values we ask for it
+df <- dplyr::tibble(x = c(1, NA, 3))
+View(df)
+
+# filter and ask for NA
+dplyr::filter(df, is.na(x) | x > 1)
+
+# ---------------- Excersice 5.2.4 ------------------
+
+# 1) Had an arrival delay of two hours or more
+dplyr::filter(flights, arr_delay >= 120)
+
+# 2) Flew to Houston (IAH or HOU)
+dest_houston <- dplyr::filter(flights, dest %in% c("IAH", "HOU"))
+View(dest_houston)
+
+# 3) Were operated by United, American or Delta
+UA_DL <- dplyr::filter(flights, carrier %in% c("AA", "UA", "DL"))
+View(UA_DL)
+
+# 4) Departed in summer (July, August, September)
+summer <- dplyr::filter(flights, month %in% c(7, 8, 9))
+View(summer)
+
+# 5) Arrived more than two hours late, but didn't leave late
+df <- dplyr::filter(flights, arr_delay > 120, dep_delay <= 0)
+View(df)
+
+# 6) Were delayed by at least an hour, but made up over 30 minutes in flight
+df <- dplyr::filter(flights, dep_delay >= 60, dep_delay - arr_delay > 30)
+View(df)
+
+# 7) Departed between midnight and 6am (inclusive)
+df <- dplyr::filter(flights, dep_time <= 600 | dep_time == 2400)
+View(df)
+
+# Find NA depature times (maybe cancelled flights)
+dplyr::filter(flights, is.na(dep_time))
